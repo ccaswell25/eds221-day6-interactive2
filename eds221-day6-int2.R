7209e8a18dba15ed15845018f82a11be817bc155
ccaswell25 <ccaswell25@gmail.com>
2023-08-21 15:02:33 -0700

    more filtering

--- eds221-day6-int2.R
+++ eds221-day6-int2.R
@@ -5,11 +5,16 @@ library(palmerpenguins)
 library(lterdatasampler)
 
 #--Highlights:--#
+#Rows#
 #Exact match : ==
 #And: & or ,
 #Or: |
 #In: %in%
 #Not equal: !=
+
+#Columns#
+#Specific columns: select()
+#Write if/else statements: case_when()
 
 
 # To look for an exact match: ==. This returns a subset of the penguins data only where island = Biscoe
@@ -58,6 +63,59 @@ view(exclude_bb_cc_pie)
 large_crabs <- pie_crab %>% filter(size > 13, site %in% c("NIB", "CC", "ZI"))
 
 
-#------#Selecting Columns#-----------#
+#------#Selecting Columns#------#
 #I want to keep or exclude columns based on whether or not they satisfy my conditions
+#dplyr::select()
 
+#Select individual columns by name, separate them by a comma
+crabs_subset <- pie_crab %>% select(latitude, size, water_temp)
+
+#Select a range of columns using :
+crabs_subset2 <- pie_crab %>% select(site:air_temp)
+
+#Select a range and an individual column
+crabs_subset3 <- pie_crab %>% select(date:water_temp, name)
+names(crabs_subset3)
+
+#----------#Mutate#-----------#
+#We can use dplyr::mutate() to add or update a column, while keeping all existing columns
+crabs_cm <-pie_crab %>%
+  mutate(size_cm = size / 10)
+
+#What happens if I use mutate to add a new column containing the mean of the size column?
+
+crabs_mean <-pie_crab %>%
+  mutate(mean_size = mean(size), na.rm = TRUE)
+
+crabs_awesome <- pie_crab %>% mutate(name = "kona is awesome")
+
+# Reminder: group_by + summarize. We can group by a variable and then summarize those groups within a column.
+mean_size_by_site <- pie_crab %>%
+  group_by(site) %>%
+  summarize(mean_size = mean(size, na.rm = TRUE),
+            sd_size = sd(size, na.rm = TRUE))
+
+# What about a group_by then mutate?
+group_mutate <- pie_crab %>%
+  group_by(site) %>%
+  mutate(mean_size = mean(size, na.rm = TRUE))
+
+#Use dplyr::case_when() to write if else statements more easily
+
+# I want to create a new column in pie_crab that contains "giant" if size is greater than 35 or "not giant" if the size is less than or equal to 20?
+
+crabs_bin <- pie_crab %>%
+  mutate(size_binned = case_when(
+    size > 20 ~ "giant",
+    size <= 20 ~ "not giant"))
+
+#Indicators of a new column region that is based on specific sites.
+
+sites_binned <- pie_crab %>%
+  mutate(region = case_when(
+    site %in% c("ZI", "CC", "PIE") ~ "Low",
+    site %in% c("BB", "NIB") ~ "Middle",
+    TRUE ~ "High"
+  ))
+
+#True says "if anything else is true...."
